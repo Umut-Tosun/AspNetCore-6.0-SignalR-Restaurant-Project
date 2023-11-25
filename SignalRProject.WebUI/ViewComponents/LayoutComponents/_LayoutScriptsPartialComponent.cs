a44838f4497de47b9023dc7b68ffd031dc153179
Umut Tosun <ses.ser2017@yandex.com>
2023-11-26 00:01:57 +0300

    Section 4 completed 26.11.2023

---
+++ SignalRProject.WebUI/ViewComponents/LayoutComponents/_LayoutScriptsPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.WebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutScriptsPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
